514bf11fad18f6e3f3f0dfb78d447044086b0393
Avery King <avery.n.king@wmich.edu>
2021-12-31 20:43:30 -0500

    Nav style update

--- frontend/src/components/MainScreen.tsx
+++ frontend/src/components/MainScreen.tsx
@@ -1,31 +1,16 @@
-import {Grid, Grow, Typography} from "@mui/material";
+import {Grid, Grow, Typography,AppBar, Container} from "@mui/material";
 import Paper from "@mui/material/Paper";
 import React from "react";
-import NavBar from "./NavBar";
+import PrimarySearchAppBar from "./NavBar";
 
 export default function MainScreen(props: any) {
     return (
-        <Grid
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            style={{minHeight: '80vh'}}
-        >
-            <Paper sx={{
-                borderRadius: '15px',
-                backgroundColor: "rgba(255,255,255,0.59)",
-                paddingTop: '10px',
-                paddingBottom: '10px'
-            }} elevation={15}>
 
-                <Grow in={true} timeout={2000}>
-                    <Grid item xs={12} sx={{padding: '100px', borderRadius: '8px'}}>
-                       <NavBar />
-                    </Grid>
-                </Grow>
-            </Paper>
-        </Grid>
+      <div style={{marginTop:-7,width:'100vw',marginLeft:-18}}>
+            <PrimarySearchAppBar/>
+        </div>
+
+
+
     )
 }
